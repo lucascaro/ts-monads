d38488aa31990d831de6ea324073701147fbeaf7
lucascaro <lucascaro@gmail.com>
2016-10-05 13:06:09 -0600

    Add either monad

--- src/Either.ts
+++ src/Either.ts
@@ -1,14 +1,23 @@
 import {Monad} from './Monad'
+import {Maybe, none, some} from './Maybe'
+
+export interface EitherCase<L,R,U,V> {
+  left(t: L): U
+  right(t: R): V
+}
 
 export interface Either<L,R> /* extends Monad<?> */{
   isLeft: boolean
   isRight: boolean
   value: L|R
-  // constructor(left: L, right: R)
   left(): L
   right(): R
+  map<U>(transform: (r: R) => U): Either<L,U>
   bind<U>(transform: (r: R) => Either<U,R>): Either<U,R>
-  map<U>(transform: (r: R) => Either<U,R>): Either<U,R>
+  ap<U>(m: Either<L, (r: R) => U>): Either<L, U>
+  join(): L | R
+  caseOf<U,V>(c: EitherCase<L,R,U,V>): U | V
+  toMaybe(): Maybe<R>
 }
 
 export interface Left<L,R> extends Either<L,R> {
@@ -22,29 +31,49 @@ export function left<L,R>(l: L): Either<L,R> {
     isLeft,
     isRight,
     value,
-    left,
-    right,
+    left: takeLeft,
+    right: takeRight,
     bind,
     map,
+    ap,
+    join,
+    caseOf,
+    toMaybe,
     toString
   }
 
-  function left(): L {
-    return value
+  function takeLeft(): L {
+    return self.value
   }
 
-  function right(): R {
+  function takeRight(): R {
     throw new Error('No right side on Either.Left')
   }
 
   function bind(transform: (r: R) => Either<L,R>): Either<L,R> {
     return self
   }
-  function map(transform: (r: R) => Either<L,R>): Either<L,R> {
-    return self
+  function map<U>(transform: (r: R) => U): Either<L,U> {
+    return left<L,U>(self.value)
   }
+  function ap<U>(m: Either<L, (r: R) => U>): Either<L, U> {
+    return left<L,U>(self.value)
+  }
+
+  function join(): L | R {
+    return self.value
+  }
+
+  function caseOf<U,V>(c: EitherCase<L,R,U,V>): U | V {
+    return c.left(self.value)
+  }
+
+  function toMaybe(): Maybe<R> {
+    return none<R>()
+  }
+
   function toString(): string {
-    return `Left(${value})`
+    return `Left(${self.value})`
   }
 
   return Object.freeze(self)
@@ -63,29 +92,51 @@ export function right<L,R>(r: R): Either<L,R> {
     isLeft,
     isRight,
     value,
-    left,
-    right,
+    left: takeLeft,
+    right: takeRight,
     bind,
     map,
+    ap,
+    join,
+    caseOf,
+    toMaybe,
     toString
   }
 
-  function left(): L {
+  function takeLeft(): L {
     throw new Error('No left side on Either.Right')
   }
 
-  function right(): R {
-    return value
+  function takeRight(): R {
+    return self.value
   }
 
   function bind(transform: (r: R) => Either<L,R>): Either<L,R> {
-    return transform(value)
+    return transform(self.value)
   }
-  function map(transform: (r: R) => Either<L,R>): Either<L,R> {
-    return transform(value)
+
+  function map<U>(transform: (r: R) => U): Either<L,U> {
+    return right<L,U>(transform(self.value))
   }
+
+  function ap<U>(m: Either<L, (r: R) => U>): Either<L, U> {
+    return right<L,U>(m.right()(self.value))
+  }
+
+  function join(): L | R {
+    return self.value
+  }
+
+  function caseOf<U,V>(c: EitherCase<L,R,U,V>): U | V {
+    return c.right(self.value)
+  }
+
+  function toMaybe(): Maybe<R> {
+    return some<R>(self.value)
+  }
+
   function toString(): string {
-    return `Right(${value})`
+    return `Right(${self.value})`
   }
 
   return Object.freeze(self)
--- src/test/Either.test.ts
+++ src/test/Either.test.ts
@@ -1,4 +1,5 @@
-import {left, right} from '../Either'
+import {EitherCase, left, right} from '../Either'
+import {Maybe} from '../Maybe'
 
 import {expect} from 'chai'
 
@@ -22,12 +23,141 @@ describe('Either', function(){
       expect(r.value).to.equal('r')
     })
   })
+
+  describe('#left()', function() {
+    it('returns left value when left', function() {
+      const l = left('error')
+      mlog.log(l)
+      expect(l.left()).to.equal('error')
+    })
+    it('throws when right', function() {
+      const l = right('some value')
+      mlog.log(l)
+      expect(l.left).to.throw()
+    })
+  })
+
+  describe('#right()', function() {
+    it('returns right value when right', function() {
+      const r = right('some value')
+      mlog.log(r)
+      expect(r.right()).to.equal('some value')
+    })
+
+    it('throws when left', function() {
+      const l = left('error')
+      mlog.log(l)
+      expect(l.right).to.throw()
+    })
+  })
+
   describe('#map()', function(){
     it('maps right + right', function(){
-      mlog.log(right('r').map((r: string) => right('Succ')))
+      const r = right('r1')
+      const f = (r: string) => r + ' Succ'
+      const m = r.map(f)
+      mlog.log(m)
+      expect(m.isRight)
+      expect(m.value).to.equal('r1 Succ')
     })
     it('maps left + right', function(){
-      mlog.log(left('error').map((r: string) => right('Error')))
+      const l = left('error')
+      const f = (r: string) => r + ' Succ'
+      const m = l.map(f)
+      mlog.log(m)
+      expect(m.isLeft)
+      expect(m.value).to.equal(l.value)
+    })
+  })
+
+  describe('#bind()', function(){
+    it('chains right + right', function(){
+      const r = right('r1')
+      const f = (r: string) => right(r + ' Succ')
+      const m = r.bind(f)
+      mlog.log(m)
+      expect(m.isRight)
+      expect(m.value).to.equal('r1 Succ')
+    })
+    it('chains left + right', function(){
+      const l = left('error')
+      const f = (r: string) => right(r + ' Succ')
+      const m = l.bind(f)
+      mlog.log(m)
+      expect(m.isLeft)
+      expect(m.value).to.equal(l.value)
+    })
+  })
+
+  describe('#ap()', function(){
+    it('applies function on the right side', function(){
+      const r = right('r1')
+      const f = right((r: string) => r + ' Succ')
+      const m = r.ap(f)
+      mlog.log(m)
+      expect(m.isRight)
+      expect(m.value).to.equal('r1 Succ')
+    })
+    it('does not apply right fn on left either', function(){
+      const l = left<string, string>('error')
+      const f = right<string, ((s: string) => string)>((r: string) => r + ' Succ')
+      const m = l.ap(f)
+      mlog.log(m)
+      expect(m.isLeft)
+      expect(m.value).to.equal(l.value)
+    })
+  })
+
+  describe('#join()', function(){
+    it('joins right, returning the value', function(){
+      const r = right('r1')
+      const j = r.join()
+      mlog.log(j)
+      expect(j).to.equal('r1')
+    })
+    it('joins left, returning an error', function(){
+      const l = left(new Error('error'))
+      const j = l.join()
+      mlog.log(j)
+      expect(j).to.be.a('error')
+      expect((j as Error).message).to.equal('error')
+    })
+  })
+
+  describe('#caseOf()', function(){
+    const l = left<Error, number>(new Error('test'))
+    const r = right<Error, number>(10)
+    const cases: EitherCase<Error, number, string, number> = {
+      left: (e: Error) => 'Error! ' + e.message,
+      right: (x: number) => x + 1
+    }
+    it('runs right case', function(){
+      const result = r.caseOf(cases)
+      mlog.log(result)
+      expect(result).to.equal(11)
+    })
+
+    it('runs left case', function(){
+      const result = l.caseOf(cases)
+      mlog.log(result)
+      expect(result).to.equal('Error! test')
+    })
+  })
+
+  describe('#toMaybe()', function(){
+    it('right returns some', function(){
+      const r = right<Error, number>(10)
+      const m: Maybe<number> = r.toMaybe()
+      mlog.log(m)
+      expect(m.isSome)
+      expect(m.value).to.equal(10)
+    })
+
+    it('left returns a none', function(){
+      const l = left<Error, number>(new Error('test'))
+      const m: Maybe<number> = l.toMaybe()
+      mlog.log(m)
+      expect(m.isNone)
     })
   })
 })
